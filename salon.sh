4fd1fa78e832b2e3e136ad8a8e0e6f531df337a4
Laleh Mousavi <56373752+MousaviLaleh@users.noreply.github.com>
2024-02-25 20:44:56 -0500

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,7 +1,4 @@
-
-#! /bin/bash
-
-PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+PSQL="psql -X -h localhost -p 54321 -U postgres --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 echo -e "Welcome to My Salon, how can I help you?\n" 
 
@@ -58,3 +55,4 @@ MAIN_MENU() {
 }
 
 MAIN_MENU
+
